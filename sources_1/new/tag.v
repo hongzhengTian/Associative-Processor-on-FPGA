05a5175521d31dfdb0f431a6e1142287f3da89e8
HONGZHENG TIAN <91035109+hongzhengTian@users.noreply.github.com>
2022-02-23 00:45:57 -0800

    solved tag_cell unused problem

--- sources_1/new/tag.v
+++ sources_1/new/tag.v
@@ -16,25 +16,19 @@ output reg [DATA_DEPTH - 1 : 0] tag_TSC
 reg [DATA_DEPTH - 1 : 0] tag_and;
 reg [DATA_DEPTH - 1 : 0] tag_and_TSC;
 
-integer i;
-
 always@(tag_A or tag_B or tag_C or tag_F_TSC) begin
-    for (i = 0; i <= DATA_DEPTH - 1; i = i + 1) begin
-        tag_and[i] = tag_A[i] & tag_B[i] & tag_C[i];
-        tag_and_TSC[i] = tag_A[i] & tag_F_TSC[i];
-    end
+    tag_and = tag_A & tag_B & tag_C;
+    tag_and_TSC = tag_A & tag_F_TSC;
 end
 
 always @(posedge clk or negedge rst) begin
-    for (i = 0; i <= DATA_DEPTH - 1; i = i + 1) begin
-        if(!rst) begin
-                tag[i] <= 0;
-                tag_TSC[i] <= 0;
-        end
-        else begin
-            tag[i] <= tag_and[i];
-            tag_TSC[i] <= tag_and_TSC[i];
-        end
+    if (!rst) begin
+        tag <= 0;
+        tag_TSC <= 0;
+    end
+    else begin
+        tag <= tag_and;
+        tag_TSC <= tag_and_TSC;
     end
 end
     
